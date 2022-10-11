3a402669fbacd930b2c9fa235aa85bffe65ba5d6
unknown <BT_4N2_13@BKTECH.LOCAL>
2022-10-11 12:19:51 -0400

    Initial commit

---
+++ src/EquationRunner.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class EquationRunner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/LinearEquation.java
@@ -0,0 +1,15 @@
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private double slope;
+    private double yInt;
+
+
+    public LinearEquation(int x1, int y1, int x2, int y2)
+    {
+        
+    }
+
+}
