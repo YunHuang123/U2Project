90d6eb0488ea6a1abab89fda2c8705527866dead
BT_4N2_13 <yhuang6817@bths.edu>
2022-10-12 12:29:33 -0400

    Worked on project

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
+        /*Scanner s = new Scanner(System.in);
+        System.out.print("Please enter your first coordinate: ");
+        String pointOne = s.nextLine();
+        System.out.print("Please enter your second coordinate: ");
+        String pointTwo = s.nextLine();
+        System.out.println("First Pair: " + pointOne);
+        System.out.println("Second Pair: " + pointTwo); */
 
+        LinearEquation one = new LinearEquation(1, 3,1,5);
+        System.out.println(one.toString());
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,9 +3,22 @@ public class LinearEquation {
     private int y1;
     private int x2;
     private int y2;
+    private int rise;
+    private int run;
     private double slope;
     private double yInt;
+    private double inverse;
+    private double distance;
+    private String info;
 
+    //constructor
+    public LinearEquation(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
     //method
     public String pointOne(int x1, int y1)
@@ -17,13 +30,32 @@ public class LinearEquation {
         return "(" + x2 + "," +  y2 + ")";
     }
 
-
-
-    //constructor
-    public LinearEquation(int x1, int y1, int x2, int y2)
+    public double calculateSlope()
     {
-
+        rise = y2 - y1;
+        run = x2 - x1;
+        slope = (double)rise / run;
+        return slope;
     }
+
+    public double yInt()
+    {
+        inverse = slope * x1 * -1;
+        yInt = inverse + y1;
+        return yInt;
+    }
+
+    public String slopeIntForm()
+    {
+        return "y = " + slope + "x " + "+ " + yInt;
+    }
+
+    public String toString()
+    {
+        info = "Slope of Line: " + calculateSlope() + "\nY-intercept: " + yInt();
+        return info;
+    }
+
 
 
 
